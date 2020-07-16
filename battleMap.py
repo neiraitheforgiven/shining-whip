d628a6a565138989cd8b016880da670c5cc6b386
John den Otter <john.denotter@computronix.com>
2020-07-16 17:01:54 -0600

    Add ability to view map

--- battleMap.py
+++ battleMap.py
@@ -150,6 +150,7 @@ class battle(object):
 
     def attack(self, unit, target):
         bf = self.battleField
+        counterattack = False
         routEnemy = False
         doubleChanceArray = []
         dex = self.getStat(unit, "Dexterity")
@@ -186,6 +187,11 @@ class battle(object):
                         self.getStat(target, "Speed")) * (1 + (
                                 (targetLuck / 10))))
                 attackTypeArray.extend(["dodge"] * dodgeSkill)
+            if self.getPower(unit, "Luck: Counterattack"):
+                counterSkill = math.floor(
+                        self.getSkill(target, "Dexterity") * (
+                                1 + targetLuck / 10))
+                attackTypeArray.extend(["counter"] * counterSkill)
             attackType = random.choice(attackTypeArray)
             if attackType == 'dodge':
                 print(f"{target.name} dodges the attack!")
@@ -235,6 +241,8 @@ class battle(object):
                     return
                 elif attackType == "routing":
                     routEnemy = True
+                elif attackType == "counter":
+                    counterattack = True
                 if routEnemy and ((i + 1) == attackCount):
                     if type(target) == playerCharacter:
                         moveTo = self.battleField.getUnitPos(target) - 1
@@ -246,6 +254,10 @@ class battle(object):
                         if moveTo <= len(self.battleField.terrainArray) - 1:
                             print(f"{target.name} was routed!")
                             self.battleField.move(target, moveTo)
+                if counterattack and ((i + 1) == attackCount):
+                    bf.checkAttack(target, bf.getunitPos(target))
+                    if unit in target.allowedAttacks:
+                        self.attack(target, unit)
 
     def battleOn(self):
         if self.egressing:
@@ -483,11 +495,15 @@ class battle(object):
     def doRound(self):
         self.turnOrder = self.determineInitiative()
         for unit in self.battleField.units:
+            unit.hasEquipped = False
             unit.movementPoints = self.getStat(unit, "Speed")
         for unit in self.turnOrder:
             # unit may have died since this loop started.
             if unit[0].hp <= 0:
                 continue
+            print("")
+            self.battleField.viewMapFromUnit(unit)
+            print("")
             endBattle = self.doTurn(unit[0])
             if endBattle:
                 return
@@ -517,11 +533,9 @@ class battle(object):
         otherUnits = ", ".join([
                 tileUnit.name for tileUnit in tile.units if tileUnit != unit])
         if type(unit) == playerCharacter:
-            allowedCommands = ["W", "w"]
+            allowedCommands = ["L", "l", "W", "w"]
+            print("Type (L) to look at the battlefield,")
             if not moved:
-                print("")
-                self.battleField.viewMapFromUnit(unit)
-                print("")
                 if type(unit) == playerCharacter:
                     maxHP = unit.maxHP()
                     maxFP = unit.stats["Faith"]
@@ -553,27 +567,22 @@ class battle(object):
                     print("Type (M) to move.")
                     allowedCommands.append("M")
                     allowedCommands.append("m")
-                    # print("Type (L) to look at a tile.")
             attackEnabled = self.battleField.checkAttack(unit, position)
             if attackEnabled:
                 self.battleField.printAttackString(unit)
                 print("Type (A) to attack.")
                 allowedCommands.append("A")
                 allowedCommands.append("a")
-                # equipEnabled = self.battleField.checkEquip(unit, position)
-                # if equipEnabled:
-                #     print("Type (E) to change equipment.")
+            if not unit.hasEquipped and not moved:
+                print("Type (E) to equip or unequip weapons.")
+                allowedCommands.append("E")
+                allowedCommands.append("e")
             spellEnabled = self.battleField.checkSpells(unit, position)
             if spellEnabled:
                 self.battleField.printSpellString(unit)
                 print("Type (S) to cast a spell.")
                 allowedCommands.append("S")
                 allowedCommands.append("s")
-            if not any([
-                    allowedCommand for allowedCommand in allowedCommands
-                    if allowedCommand not in ("W", "w")]):
-                print(f"{unit.name} waited.")
-                return
             vocalEnabled = self.battleField.checkVocal(unit)
             if vocalEnabled:
                 print("Type (V) to make a vocal attack.")
@@ -592,7 +601,7 @@ class battle(object):
                         moveTo = None
                 self.battleField.move(unit, moveTo)
                 self.doTurn(unit, True)
-            if command in ("A", "a"):
+            elif command in ("A", "a"):
                 attackTarget = None
                 while attackTarget not in [
                         unit.allowedAttacks.index(target)
@@ -602,7 +611,57 @@ class battle(object):
                     except ValueError:
                         attackTarget = None
                 self.doAttack(unit, attackTarget)
-            if command in ("S", "s"):
+            elif command in ("E", "e"):
+                allowedEquipment = [
+                        item for item in self.game.inventory
+                        if unit.canEquip(item) and not item.equippedBy]
+                itemToEquip = None
+                while itemToEquip not in [
+                        allowedEquipment.index(item)
+                        for item in allowedEquipment] and (itemToEquip not in (
+                        len(allowedEquipment), len(allowedEquipment) + 1)):
+                    for item in allowedEquipment:
+                        print(
+                                f"({allowedEquipment.index(item)}) "
+                                f"{item.name}")
+                        self.printEstimatedValue(unit, item)
+                    print(f"({len(allowedEquipment)}) Go barehanded.")
+                    self.printEstimatedValue(unit)
+                    print(f"({len(allowedEquipment) + 1}) Never mind.")
+                    try:
+                        itemToEquip = int(input("Type a number to equip: "))
+                    except ValueError:
+                        itemToEquip = None
+                if itemToEquip == len(allowedEquipment):
+                    if unit.equipment:
+                        incumbent = unit.equipment
+                        incumbent.equippedBy = None
+                    unit.equipment = None
+                    unit.hasEquipped = True
+                    print(f"{unit.name} put away their weapon.")
+                    itemToEquip = None
+                elif itemToEquip == len(allowedEquipment) + 1:
+                    pass
+                elif itemToEquip is not None:
+                    self.game.equipOnCharacter(
+                            allowedEquipment[itemToEquip], unit)
+                    itemToEquip = None
+                    unit.hasEquipped = True
+                    print()
+                self.doTurn(unit, True)
+            elif command in ("L", "l"):
+                tileChoice = None
+                while tileChoice not in ([
+                        self.battleField.terrainArray.index(lookTile)
+                        for lookTile in self.battleField.terrainArray]):
+                    try:
+                        tileChoice = int(input(
+                                "Type the number of a tile to look from: "))
+                    except ValueError:
+                        tileChoice = None
+                self.battleField.viewMap(tileChoice)
+                self.doTurn(unit, moved)
+            elif command in ("S", "s"):
                 spellChoice = None
                 spellTarget = None
                 spellKeys = list(unit.allowedSpells.keys())
@@ -623,9 +682,9 @@ class battle(object):
                     except ValueError:
                         spellTarget = None
                 self.castSpell(unit, spellToCast, spellTarget)
-            if command in ("V", "v"):
+            elif command in ("V", "v"):
                 self.doVocalAttack(unit)
-            if command in ("W", "w"):
+            elif command in ("W", "w"):
                 return
         elif type(unit) == monster:
             print("")
@@ -766,6 +825,10 @@ class battle(object):
         if self.getPower(unit, "Magic: Cost Reduction I"):
             cost = math.ceil(cost * 0.75)
         return cost
+
+    def printEstimatedValue(self, unit, equipment=None):
+        bf = self.battleField
+        bf.printEstimatedValue(unit, equipment)
 
 
 class battleTile(object):
@@ -1294,6 +1357,56 @@ class battleField(object):
         attackString += ", ".join(attackStringAdds)
         print(attackString + ".")
 
+    def printEstimatedValue(self, unit, equipment=None):
+        valueString = "  "
+        incumbent = unit.equipment
+        unitDamage = max(
+                self.getStat(unit, "Strength"),
+                self.getStat(unit, "Dexterity"))
+        if incumbent:
+            fromDamage = incumbent.damage + unitDamage
+            fromFaith = incumbent.fp + self.getStat(unit, "Faith")
+            fromMagic = incumbent.mp + self.getStat(unit, "Intelligence")
+            fromType = incumbent.type
+        else:
+            fromDamage = unitDamage
+            fromFaith = self.getStat(unit, "Faith")
+            fromMagic = self.getStat(unit, "Intelligence")
+            fromType = "Unarmed Attack"
+        if self.getPower(unit, f"{fromType}: Increased Damage I"):
+            fromDamage *= 1.3
+        if self.getPower(unit, f"{fromType}: Increased Damage II"):
+            fromDamage *= 1.3
+        if self.getPower(unit, f"{fromType}: Increased Damage III"):
+            fromDamage *= 1.3
+        if self.getPower(unit, f"{fromType}: Increased Damage II"):
+            fromDamage *= 1.3
+        if equipment:
+            toDamage = equipment.damage + unitDamage
+            toFaith = equipment.fp + self.getStat(unit, "Faith")
+            toMagic = equipment.mp + self.getStat(unit, "Intelligence")
+            toType = equipment.type
+        else:
+            toDamage = unitDamage
+            toFaith = self.getStat(unit, "Faith")
+            toMagic = self.getStat(unit, "Intelligence")
+            toType = "Unarmed Attack"
+        if self.getPower(unit, f"{toType}: Increased Damage I"):
+            toDamage *= 1.3
+        if self.getPower(unit, f"{toType}: Increased Damage II"):
+            toDamage *= 1.3
+        if self.getPower(unit, f"{toType}: Increased Damage III"):
+            toDamage *= 1.3
+        if self.getPower(unit, f"{toType}: Increased Damage II"):
+            toDamage *= 1.3
+        if fromDamage != toDamage:
+            valueString += f"Damage: {fromDamage}-->{toDamage}  "
+        if fromFaith != toFaith:
+            valueString += f"FP: {fromFaith}-->{toFaith}  "
+        if fromMagic != toMagic:
+            valueString += f"MP: {fromMagic}-->{toMagic}  "
+        print(valueString)
+
     def printSpellString(self, unit):
         spellString = f"{unit.name} can cast "
         spellStringAdds = []
@@ -1526,7 +1639,7 @@ class game(object):
                 "\"We have to go north to inform Her Majesty of her father's "
                 "death.\"")
         print("\"I'm Khris. The priesthood is still loyal to Yatahal.\"")
-        recruit = playerCharacter("Khris", "Kyantol", "Prophet", chatter, 4)
+        recruit = playerCharacter("Khris", "Kyantol", "Priest", chatter, 4)
         self.equipOnCharacter(
                 equipment("Staffs", "Wooden Staff", 80, 0, 0, 1, 3, 3),
                 recruit)
@@ -1592,14 +1705,19 @@ class game(object):
             if pc.equipment:
                 incumbent = pc.equipment
                 incumbent.equippedBy = None
+            if equipment.equippedBy:
+                incumbent = equipment.equippedBy
+                incumbent.equipment = None
             equipment.equippedBy = pc
             pc.equipment = equipment
+            if equipment not in self.inventory:
+                self.inventory.append(equipment)
             print(f"{pc.name} equipped the {equipment.name}.")
 
     def getSellPrice(self, item):
         equipString = f"Equip: {item.type}"
         fame = max([
-                pc.stat["Fame"] for pc in self.playerCharacters
+                pc.stats["Fame"] for pc in self.playerCharacters
                 if equipString in pc.powers])
         amount = math.floor(item.price * (0.1 + (fame / 100)))
         return amount
