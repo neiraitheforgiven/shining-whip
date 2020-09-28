cbb7043bb373e6f48459cf90cf3c7465991eca00
John den Otter <john.denotter@computronix.com>
2020-09-28 08:43:44 -0600

    Nerf Zombies but make them luckier

--- characters.py
+++ characters.py
@@ -89,7 +89,9 @@ class monster(object):
             self.shortName = "Knight"
         elif name == "Zombie":
             self.level = 5
-            stats = {"Strength": 17, "Dexterity": 7, "Speed": 5, "Stamina": 10}
+            stats = {
+                    "Strength": 15, "Dexterity": 7, "Speed": 5, "Stamina": 10,
+                    "Luck": 12}
             self.setStats(6, **stats)
             self.moveProfile = moveProfile or "SlowAdvance"
             self.attackProfile = attackProfile or "Random"
