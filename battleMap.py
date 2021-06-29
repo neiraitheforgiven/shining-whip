5471cbc8ae31cb2563c7b165806cbe5825bb5418
John den Otter <john.denotter@computronix.com>
2021-06-29 14:46:58 -0600

    Fix crash when calculating murder target

--- battleMap.py
+++ battleMap.py
@@ -1056,7 +1056,7 @@ class battle(object):
             elif monster.focusProfile == "Murderous":
                 if any(
                         target for target in monster.allowedAttacks
-                        if target.hp < math.ceil(target.maxHP / 2)):
+                        if target.hp < math.ceil(target.maxHP() / 2)):
                     self.enterFocus(monster)
             elif monster.focusProfile == "Patient":
                 # focus will be 3000
