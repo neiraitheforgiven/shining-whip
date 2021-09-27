732a0f7198ecd17577faaac651ff456358fe5158
John den Otter <john.denotter@computronix.com>
2021-09-27 15:02:31 -0600

    Wow allies at position was stupid broken

--- battleMap.py
+++ battleMap.py
@@ -2707,7 +2707,7 @@ class battleField(object):
         return [
             unit
             for unit in self.terrainArray[position].units
-            if type(unit) != type(me) and self.canBeTarget(unit) and (unit != me)
+            if type(unit) == type(me) and self.canBeTarget(unit) and (unit != me)
         ]
 
     def calculatePossibleMovement(
@@ -3442,10 +3442,11 @@ class battleField(object):
     def getFameBonus(self, unit):
         position = self.getUnitPos(unit)
         print(f"debug: {unit.name}'s position for getFameBonus is {position}")
-        if not position:
-            print(f"debug: {unit.name} has no {position}")
+        if position is None:
+            print(f"debug: {unit.name} has no position")
             return 1
         allyFame = [ally.getFame() for ally in self.alliesAtPosition(unit, position)]
+        print(f"debug: {allyFame}")
         if any(allyFame):
             return 1 + round((max(allyFame) / 100.0), 2)
         else:
